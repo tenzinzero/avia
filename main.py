269a25ecb4bb39db058e82c44a88b66db7448677
Tenzin <tenzinzero05@mail.ru>
2022-12-02 20:05:24 +0700

    completetd fisting ass

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ import numpy as np
 import pygame
 import random
 import os
-import numpy
 
 
-
-'''WIDTH = KOEF[0] * MASH
-HEIGHT = KOEF[1] * MASH'''
 FPS = 60
+sr_znach = [0, 0]
 
 # Задаем цвета
 WHITE = (255, 255, 255)
@@ -37,20 +34,23 @@ def load_image(name, x, y, colorkey=None):
 def fps(screen, clock, WIDTH):
     front = pygame.font.SysFont('Arial', 30, bold=True)
     display_fps = str(int(clock.get_fps()))
+    sr_znach[0] += int(display_fps)
+    sr_znach[1] += 1
     render = front.render(display_fps, 0, (255, 0, 0))
     screen.blit(render, (WIDTH - 60, 30))
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, player_image, pos_x, pos_y):
-        super().__init__(all_sprites)
+        super().__init__(somalet_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(pos_x, pos_y)
-        self.sostoniye = ''
+        '''self.sostoniye = ''
         self.tim_down = 1
         self.tim_up = 1
         self.speeding_up = 1
-        self.speeding_down = 2
+        self.speeding_down = 2'''
+        self.speed = 5
         # self.pozitsiya = self.rect.y
         # self.mask_bird = pygame.mask.from_surface(self.image2)
         self.fly_or_not_fly = True
@@ -59,25 +59,27 @@ class Player(pygame.sprite.Sprite):
         for i in collision:
             if pygame.sprite.collide_mask(self, i):
                 self.fly_or_not_fly = False
-        if self.fly_or_not_fly:
-            self.movement()
 
+        '''if self.fly_or_not_fly:
+            self.movement()'''
 
     def movement(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.rect.y -= 2
-        if keys[pygame.K_a]:
-            self.rect.x -= 2
-        if keys[pygame.K_d]:
-            self.rect.x += 2
-        if keys[pygame.K_s]:
-            self.rect.y += 2
+        if self.fly_or_not_fly:
+            keys = pygame.key.get_pressed()
+            speed = self.speed
+            if keys[pygame.K_w]:
+                self.rect.y -= speed
+            if keys[pygame.K_a]:
+                self.rect.x -= speed
+            if keys[pygame.K_d]:
+                self.rect.x += speed
+            if keys[pygame.K_s]:
+                self.rect.y += speed
 
 
 class Radar(pygame.sprite.Sprite):
     def __init__(self, radarich, pos_x, pos_y):
-        super().__init__(all_sprites)
+        super().__init__(somalet_sprites)
         self.image = radarich
         self.rect = self.image.get_rect().move(pos_x, pos_y)
 
@@ -90,66 +92,84 @@ class Obstacles(pygame.sprite.Sprite):
         super().__init__(obstac_sprites)
         self.image = obstac
         self.rect = self.image.get_rect().move(pos_x, pos_y)
+        self.offset_for_obs_x = main_somal_x
+        self.offset_for_obs_y = main_somal_y
 
-    def update(self):
+    def update(self, offset_x, offset_y):   #offset = смещение
+        x_offset = self.offset_for_obs_x - offset_x
+        y_offset = self.offset_for_obs_y - offset_y
+
+        #присваиваеи к прошлому смещению будущее смещение
+        self.offset_for_obs_x = offset_x
+        self.offset_for_obs_y = offset_y
+
+        self.rect.x += x_offset
+        self.rect.y += y_offset
+
         self.rect.x += -1
 
 
 def draw_obs():
     kol_obs = []
-    for i in range(KOL_OBS):
-        kol_obs.append([obstacles[i].rect.x / KOF_X, obstacles[i].rect.y / KOF_Y])
+    for i in obstac_sprites:
+        W_H = i.rect
+        kol_obs.append([W_H.x / KOF_X, W_H.y / KOF_Y])
     for i in range(KOL_OBS):
         x = kol_obs[i][0]
         y = kol_obs[i][1]
-        pygame.draw.circle(screen, WHITE, (x, h - kof_2 + y), 5)
-
-
+        if (x <= 0 or y >= 0) and x < kof_1_x and y < kof_2_y:
+            pygame.draw.circle(screen, WHITE, (x, HEIGHT - kof_2_y + y), 5)
 
 
 # Создаем игру и окно
 pygame.init()
 pygame.mixer.init()
-#window
+
+#окно
 infoObject = pygame.display.Info()
-w = infoObject.current_w
-h = infoObject.current_h
-screen = pygame.display.set_mode((w, h))
-#screen = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH = infoObject.current_w
+HEIGHT = infoObject.current_h
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
 #fps
 clock = pygame.time.Clock()
+
 #name window
 pygame.display.set_caption("OVD")
-all_sprites = pygame.sprite.Group()
+
+#sprite
+somalet_sprites = pygame.sprite.Group()
 obstac_sprites = pygame.sprite.Group()
+
 #main samolet
 main_obj = 'somalet.png'
-player = Player(load_image(main_obj, 150, 75), 300, 300)
+main_somal_x = 300
+main_somal_y = 300
+player = Player(load_image(main_obj, 150, 75), main_somal_x, main_somal_y)
+
 # препятствия
 obst = 'another_somalet.png'
-obstacles = []
+#obstacles = []
 
 KOL_OBS = 6
 for i in range(KOL_OBS):
     a = random.randint(50, 1000)
     b = random.randint(500, 1300)
-    obstacles.append(Obstacles(load_image(obst, 200, 100), b, a))
+    obstac_sprites.add(Obstacles(load_image(obst, 200, 100), b, a))
 
 
 #радар
 radar = 'radar.png'
 #radar_blit = load_image(radar, 200, 200)
-kof_1 = 300
-kof_2 = 200
-KOF_X = w / kof_1
-KOF_Y = h / kof_2
-obj_rad = Radar(load_image(radar, kof_1, kof_2), 0, h - 200)
+kof_1_x = 300
+kof_2_y = 200
+KOF_X = WIDTH / kof_1_x
+KOF_Y = HEIGHT / kof_2_y
+obj_rad = Radar(load_image(radar, kof_1_x, kof_2_y), 0, HEIGHT - 200)
 #all_sprites.add(player)
 
-fon = load_image('fon_sky.jpg', w, h)
-main_obj = 'somalet.png'
-
-
+#fon
+fon = load_image('fon_sky.jpg', WIDTH, HEIGHT)
 
 # Цикл игры
 running = True
@@ -166,22 +186,26 @@ while running:
                 running = False
 
     # Обновление
-    #player.movement()
     if player.fly_or_not_fly:
-        player.update(obstacles) #!!!!!!!!!!!
-        obstac_sprites.update()
+        player.update(obstac_sprites) #!!!!!!!!!!!
+        #проверяем движение
+        player.movement()
+        obstac_sprites.update(player.rect.x, player.rect.y)
+        #obstac_sprites.update()
     # Рендеринг
     screen.blit(fon, (0, 0))
     obstac_sprites.draw(screen)
-    all_sprites.draw(screen)
-    #draw obst
+    somalet_sprites.draw(screen)
+    #Рисуем припятствия
     draw_obs()
-    fps(screen, clock, w)
+    #Рисуем fps
+    fps(screen, clock, WIDTH)
     #screen.blit(radar_blit, (0, HEIGHT - 200))
 
     # После отрисовки всего, переворачиваем экран
     pygame.display.flip()
     clock.tick(60)
-
+print(sr_znach)
+print(sr_znach[0] / sr_znach[1])
 pygame.quit()
 
